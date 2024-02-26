85a48678644f6cede0c9c2f2af8ebe938e8c7d22
anujkumarchoudhary <timetechapps1@gmail.com>
2024-02-26 05:14:57 -0800

    changes by anuj

---
+++ src/components/tableData/bookingTable/BookingTable.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import styles from "./BookingTable.module.scss";
+import {
+  Box,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+import HeadingText from "../../HeadingText/HeadingText";
+
+export interface IBookingTable {
+  tableTitle: any;
+  tableData: any;
+}
+
+const BookingTable = ({ tableTitle, tableData }: IBookingTable) => {
+  const [query, setQuery] = useState("")
+
+  const formattedDate = (idx: any) => {
+    const date = new Date(idx)
+    return date.toLocaleDateString()
+  };
+  return (
+    <Grid className={styles.bookingTableContainer}>
+      <HeadingText
+        IsAction={true}
+        IsSearchBox={true}
+        setQuery={setQuery}
+        heading={"All Booking"}
+        name="New Booking"
+      />
+      <TableContainer className={styles.tableContainer}>
+        <Table>
+          <TableHead style={{ backgroundColor: "#00ACB2" }}>
+            <TableRow>
+              {tableTitle.map((item: any) => {
+                return (
+                  <TableCell style={{ color: "#000000", textAlign: "left", fontSize: 15, fontWeight: 600 }}>{item.title}</TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData && tableData.filter((employee: { name: string }) => {
+              return (
+                query === "" ||
+                (employee.name
+                  ?.toLowerCase()
+                  ?.includes(query.toLowerCase()) ??
+                  false)
+              );
+            })
+              .map((item: any, idx: any) => {
+                return (
+                  <TableRow key={idx}>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.userId?.name}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.userId?.email}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{formattedDate(item?.createdAt)}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.payment?.advancePayment?.amount.toFixed(2)}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.payment?.advancePayment?.method}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.payment?.pendingAmount?.amount.toFixed(2)}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.totalPayableAmount.toFixed(2)}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.destination?.cityName}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.destination?.pincode}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.destination?.state}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.pickupDate?.from}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.pickupDate?.date}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.dropoffDate?.from}</TableCell>
+                    <TableCell sx={{ textAlign: "left" }}>{item?.dropoffDate?.date}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
+  );
+};
+
+export default BookingTable;
--- src/pages/VehiclesPage/VehiclesPage.tsx
+++ src/pages/VehiclesPage/VehiclesPage.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment, useEffect, useState } from "react";
 import styles from "./VehiclesPage.module.scss";
-import { Box, Grid, ListItemButton, Typography } from "@mui/material";
+import { Box, Grid, ListItemButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import CommonHeading from "../../components/common/CommonHeading/CommonHeading";
-import EmployeeTable from "../../components/tableData/employeeTable/EmployeeTable";
+import EmployeeTable from "../../components/tableData/bookingTable/BookingTable";
 // import data from "./data.json";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -18,32 +18,15 @@ import * as XLSX from 'xlsx';
 
 
 const VehiclesPage = () => {
-  const navigation = useNavigate()
-  const [payrollModal, setPayrollModal] = useState(false);
-  const [downloadModal, setDownloadModal] = useState(false)
-  const handleClose = () => { setPayrollModal(false); setDownloadModal(false) };
-  const [inputData, setInputData] = useState<any>({
-    name: "",
-    email: "",
-    emp_id: "",
-    branch: "",
-    department: "",
-    designation: "",
-    dateOfJoin: ""
-  });
-  const [payrollVal, setPayrollVal] = useState({ employeeId: "", month: "", year: "" })
-  const [query, setQuery] = useState("");
-  const [employeeData, setEmployeeData] = useState<any>([]);
-  const [loading, setLoading] = useState(false)
-  const [downloadId, setDownloadId] = useState()
-  const [srStructure, setSrStrucure] = useState<any>()
+  const [loading, setLoading] = useState(false);
+  const [vehiclesData, setVehiclesData] = useState<any>();
 
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await axios.get(`https://hrms-server-ygpa.onrender.com/api/v1/user/get`)
-      const data = response.data.userData
-      setEmployeeData(data);
+      const response = await axios.get(`https://qbus-traveler.onrender.com/api/v1/vehicles/available/list?available=true`)
+      console.log(response.data.vehicleTypes, "response")
+      setVehiclesData(response.data.vehicleTypes)
 
     } catch (error) {
       console.error("Error fetching employee data:", error);
@@ -52,120 +35,8 @@ const VehiclesPage = () => {
     }
   };
 
-  const handlePayrollModal = async (idx: any) => {
-    setPayrollModal((preState: any) => ({ ...preState, [idx]: !preState[idx] }));
-    setPayrollVal({ ...payrollVal, employeeId: idx });
-  };
-  const handleChangePayroll = (e: any) => {
-    const { name, value } = e.target;
-    setPayrollVal({ ...payrollVal, [name]: value });
-  }
-  const handleCreatePayroll = async () => {
-    const loginedUserString: any = localStorage.getItem("loginedUser")
-    const loginedUser = JSON.parse(loginedUserString)
-    const { token } = loginedUser
-    if (payrollVal.month === "") {
-      toast.error("Please fill month");
-      return;
-    } else if (payrollVal.year === "") {
-      toast.error("Please fill year")
-      return;
-    }
-
-    try {
-      const response = await axios.post(`https://hrms-server-ygpa.onrender.com/api/v1/payroll/create`, payrollVal,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-      if (response.status === 200) {
-        toast.success("Payroll created successfuly")
-        setPayrollModal(false)
-      }
-    }
-    catch (err) {
-      console.log(err)
-    }
-
-
-  }
-
-  const handlePayrollDownloadModal = (idx: any) => {
-    setDownloadModal((preState: any) => ({ ...preState, [idx]: !preState[idx] }))
-    console.log(idx, "idx")
-    setDownloadId(idx)
-  }
-  const handleDownloadPaySlip = () => {
-    const input: any = document.getElementById('userData');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'PNG', 0, 0, 210, 297);
-        pdf.save('userData.pdf');
-      });
-
-  }
-
-  const getAllSalaryStructure = async () => {
-    const loginedUserString: any = localStorage.getItem("loginedUser")
-    const loginedUser = JSON.parse(loginedUserString)
-    const { token } = loginedUser
-    try {
-      const response = await axios.get(`https://hrms-server-ygpa.onrender.com/api/v1/salary/download/all-users-salary`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-      const data = response.data.salaryStructures;
-      setSrStrucure(data)
-      console.log(response.data.salaryStructures, "responsesla")
-    }
-    catch (err) {
-      console.log(err)
-    }
-
-  }
-  const handleDownload = async () => {
-    try {
-      const response = await axios.get(`https://hrms-server-ygpa.onrender.com/api/v1/payroll/download/monthly-payroll/${downloadId}?month=${payrollVal.month}`
-      )
-      if (response.status === 200) {
-        toast.success("success")
-        const payrollData = response.data.payroll;
-        localStorage.setItem("payrollData", JSON.stringify(payrollData))
-        navigation('/pay-slip-preview')
-      }
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-  const handleDownloadPayrollData = () => {
-    const userData: any[] = [];
-
-    srStructure?.forEach((strcture: any) => {
-      const { employeeId, basicSalary: netPay, createdAt, daPercentage, esiPercentage, hraPercentage, pfPercentage, ptaxDeduction, specialAllowance, travelAllowance, } = strcture;
-
-      const formateDate = new Date(createdAt).toLocaleString()
-
-      userData.push({ name: employeeId?.name, email: employeeId?.email, mobile: employeeId?.mobile, role: employeeId?.role, department: employeeId?.department, bankName: employeeId?.bankName, bankAccount: employeeId?.bankAccount, branch: employeeId?.branch, IFSC: employeeId?.IFSC, esic: employeeId?.esic, uanNumber: employeeId?.uanNumber, netPay, createdAt: formateDate, daPercentage, esiPercentage, hraPercentage, pfPercentage, ptaxDeduction, specialAllowance, travelAllowance, });
-    });
-
-    const ws = XLSX.utils.json_to_sheet(userData);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, 'table_data.xlsx');
-
-
-  }
-
-
   useEffect(() => {
     fetchData();
-    getAllSalaryStructure();
   }, []);
 
 
@@ -173,37 +44,33 @@ const VehiclesPage = () => {
     <Fragment>
       <Grid className={styles.employeePageContainer}>
         <CommonHeading
-          heading={""}
+          heading={"Vehicles"}
           IsHeadingAction={false}
         />
-        {/* <EmployeeTable
-          heading={"Pay Slip Management"}
-          tableTitle={data.tableTitle}
-          tableData={employeeData}
-          handlePayrollModal={handlePayrollModal}
-          handlePayrollDownload={handlePayrollDownloadModal}
-          handleDownload={handleDownloadPayrollData}
-          setQuery={setQuery}
-          query={query}
-        /> */}
-        <CreatePayrollModal
-          open={payrollModal}
-          heading={"Create Payroll"}
-          name="Submit"
-          payrollVal={payrollVal}
-          handleCreate={handleCreatePayroll}
-          handleClose={handleClose}
-          handleChange={handleChangePayroll}
-        />
-        <CreatePayrollModal
-          open={downloadModal}
-          name="Preview"
-          heading={"Download Pay Slip"}
-          payrollVal={payrollVal}
-          handleCreate={handleDownload}
-          handleClose={handleClose}
-          handleChange={handleChangePayroll}
-        />
+        <TableContainer>
+          <Table>
+            <TableHead sx={{ backgroundColor: '#00ACB2' }}>
+              <TableCell sx={{ textAlign: "center" }}>Image</TableCell>
+              <TableCell sx={{ textAlign: "center" }}>Name</TableCell>
+              <TableCell sx={{ textAlign: "center" }}>No of Seats</TableCell>
+              <TableCell sx={{ textAlign: "center" }}>Rate Per Km</TableCell>
+            </TableHead>
+            <TableBody>
+              {vehiclesData && vehiclesData.map((item: any) => {
+                return (
+                  <TableRow>
+                    <TableCell sx={{ textAlign: "center" }}>
+                      <img src={item.img} width={100} height={80} />
+                    </TableCell>
+                    <TableCell sx={{ textAlign: "center" }}>{item.name}</TableCell>
+                    <TableCell sx={{ textAlign: "center" }}>{item.noOfSeats}</TableCell>
+                    <TableCell sx={{ textAlign: "center" }}>{item.ratePerKm}</TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
         <ToastContainer />
       </Grid>
     </Fragment>
